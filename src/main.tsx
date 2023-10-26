fb8a9f5f05e43a9cf148c6c3c95bd9af318ddde6
Victor Stepanov <vix522430@bk.ru>
2023-10-26 12:38:27 +0300

    feat: добавил redux-toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/index.css';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './pages/routes';
 
+import { store } from './store';
+import { theme } from './theme';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
